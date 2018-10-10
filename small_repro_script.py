7b04a7abb2c3a87eedb5407f1bbfd57032214547
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2018-10-10 18:58:12 +0500

    Refactor small_repro_script

--- small_repro_script.py
+++ small_repro_script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 
 import ccmlib.cluster
-import pycassa
+from pycassa import SystemManager, ConnectionPool, ColumnFamily, SIMPLE_STRATEGY, cassandra
 
 values = list(map(str, range(10)))
 keyspace, table = 'test_keyspace', 'test_table'
@@ -9,21 +9,23 @@ keyspace, table = 'test_keyspace', 'test_table'
 cluster = ccmlib.cluster.Cluster('.', 'thrift-test', partitioner='RandomPartitioner', cassandra_version='3.11.2')
 cluster.set_configuration_options({'start_rpc': 'true', 'hinted_handoff_enabled': 'false'})
 cluster.populate(3, debug=True).start()
-sys = pycassa.SystemManager('127.0.0.1')
-sys.create_keyspace(keyspace, pycassa.SIMPLE_STRATEGY, {'replication_factor': '3'})
+sys = SystemManager('127.0.0.1')
+sys.create_keyspace(keyspace, SIMPLE_STRATEGY, {'replication_factor': '3'})
 sys.create_column_family(keyspace, table)
-cluster.nodelist()[2].stop()
-pool = pycassa.ConnectionPool(keyspace, server_list=['127.0.0.1', '127.0.0.2'], timeout=0.5)
-cf = pycassa.ColumnFamily(pool, table)
-for value in values:
-    cf.insert(value, {'value': value}, write_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.QUORUM)
-cluster.nodelist()[2].start()
-cluster.nodelist()[2].wait_for_thrift_interface()
 
-pool = pycassa.ConnectionPool(keyspace, server_list=['127.0.0.3'], timeout=60 * 60 * 1000)
-cf = pycassa.ColumnFamily(pool, table)
-print(cf.multiget(values, read_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.QUORUM))
-pool = pycassa.ConnectionPool(keyspace, server_list=['127.0.0.1'], timeout=60 * 60 * 1000)
-cf = pycassa.ColumnFamily(pool, table)
-print(cf.multiget(values, read_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.ONE))
+# Imitate temporary node unavailability that cause inconsistency in data across nodes
+failing_node = cluster.nodelist()[2]
+failing_node.stop()
+cf = ColumnFamily(ConnectionPool(keyspace, server_list=['127.0.0.1']), table)
+for value in values:
+    cf.insert(value, {'value': value}, write_consistency_level=cassandra.ttypes.ConsistencyLevel.QUORUM)
+failing_node.start()
+failing_node.wait_for_thrift_interface()
+
+cf = ColumnFamily(ConnectionPool(keyspace, server_list=['127.0.0.3']), table)
+# Returns many Nones records
+print(cf.multiget(values, read_consistency_level=cassandra.ttypes.ConsistencyLevel.QUORUM).values())
+cf = ColumnFamily(ConnectionPool(keyspace, server_list=['127.0.0.1']), table)
+# All returned records are fine
+print(cf.multiget(values, read_consistency_level=cassandra.ttypes.ConsistencyLevel.ONE).values())
 cluster.stop()
