dc0fb375a5555c338a63809fa785c1cc3f056fbf
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2018-10-10 15:06:57 +0500

    Slightly rewrite small repro script

--- small_repro_script.py
+++ small_repro_script.py
@@ -3,23 +3,27 @@
 import ccmlib.cluster
 import pycassa
 
+values = list(map(str, range(100)))
+keyspace, table = 'test_keyspace', 'test_table'
 
 cluster = ccmlib.cluster.Cluster('.', 'thrift-test', partitioner='RandomPartitioner', cassandra_version='3.11.2')
 cluster.set_configuration_options({'start_rpc': 'true', 'hinted_handoff_enabled': 'false'})
 cluster.populate(3, debug=True).start()
 sys = pycassa.SystemManager('127.0.0.1')
-sys.create_keyspace('test_keyspace', pycassa.SIMPLE_STRATEGY, {'replication_factor': '3'})
-sys.create_column_family('test_keyspace', 'test_table')
+sys.create_keyspace(keyspace, pycassa.SIMPLE_STRATEGY, {'replication_factor': '3'})
+sys.create_column_family(keyspace, table)
 cluster.nodelist()[2].stop()
-pool = pycassa.ConnectionPool('test_keyspace', server_list=['127.0.0.1', '127.0.0.2'], timeout=0.5)
-cf = pycassa.ColumnFamily(pool, 'test_table')
-for value in ['cassandra', 'thrift']:
+pool = pycassa.ConnectionPool(keyspace, server_list=['127.0.0.1', '127.0.0.2'], timeout=0.5)
+cf = pycassa.ColumnFamily(pool, table)
+for value in values:
     cf.insert(value, {'value': value})
 cluster.nodelist()[2].start()
 cluster.nodelist()[2].wait_for_thrift_interface()
 
-pool = pycassa.ConnectionPool('test_keyspace', server_list=['127.0.0.3'], timeout=60 * 60 * 1000)
-cf = pycassa.ColumnFamily(pool, 'test_table')
-print(cf.multiget([b'cassandra', b'thrift'], read_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.QUORUM))
-print(cf.multiget([b'cassandra', b'thrift'], read_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.ONE))
+pool = pycassa.ConnectionPool(keyspace, server_list=['127.0.0.3'], timeout=60 * 60 * 1000)
+cf = pycassa.ColumnFamily(pool, table)
+print(cf.multiget(values, read_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.QUORUM))
+pool = pycassa.ConnectionPool(keyspace, server_list=['127.0.0.1'], timeout=60 * 60 * 1000)
+cf = pycassa.ColumnFamily(pool, table)
+print(cf.multiget(values, read_consistency_level=pycassa.cassandra.ttypes.ConsistencyLevel.ONE))
 cluster.stop()
